e20c4f0db62706facfe3d11676cb6751e8f441bc
Natalie Groeneboom <nataliegroeneboom@users.noreply.github.com>
2018-09-11 15:14:07 +0100

    create tabs

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /** @format */
 
+import React, { Component } from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import MainApp from './src/MainApp';
 
-AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.registerComponent('Cookbook', () => MainApp);
